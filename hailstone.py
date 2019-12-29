479e236d6f40c768c9aa0cba25c80d7dac6746a8
Claudio Heckler <claudio.heckler@hp.com>
2019-12-28 23:13:43 -0300

    Keep track of the number of iterations, prints successive records achieved

--- hailstone.py
+++ hailstone.py
@@ -16,33 +16,37 @@ def export_results(n, graph):
     f = open("out/graph_%d.dot" % n,"w+")
     f.write("digraph {\n")
     for key in graph:
-        f.write("\t%d -> %d\n" % (key, graph[key]))
+        f.write("\t%d -> %d\n" % (key, graph[key][0]))
     f.write("}\n")
 
 
-# initial conditions:
-graph = {1:1}
+# graph:  { number: (next, total_num_iter) }
+graph = {1: (1, 0) }
+
 target = 2
+max_iter = 0
 
 if not os.path.exists("out"):
     os.makedirs("out")
 
-while (target < 100000):
+while (True):
     target = target + 1
     n = target
+    num_iter = 0
 
     done = False
     while (not done):
         m = n
+        num_iter = num_iter + 1
         if (n % 2) == 0:
             n = n / 2
         else:
             n = (n*3) + 1
         if (graph.has_key(n)):
-            graph[m] = n
+            graph[m] = (n, 1 + graph[n][1])
             done = True
-    if (target % 1000) == 0:
-        print(target, m)
+            num_iter = num_iter + graph[n][1]
+            if ( num_iter > max_iter):
+                max_iter = num_iter
+                print("%d: %d" % (target, max_iter) )
 
-    if (target % 1000) == 0:
-        export_results(target, graph)
